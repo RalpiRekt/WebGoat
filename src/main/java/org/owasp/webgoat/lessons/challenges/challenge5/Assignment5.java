4ce546fcd254fe0c3825248fc194952cbdae0c3e
saultj <sauldelasgalaxias@gmail.com>
2026-03-01 22:34:31 +0100

    FIX: Consulta SQL arreglada utilizando PrepareStatement

--- src/main/java/org/owasp/webgoat/lessons/challenges/challenge5/Assignment5.java
+++ src/main/java/org/owasp/webgoat/lessons/challenges/challenge5/Assignment5.java
@@ -57,17 +57,17 @@ public class Assignment5 extends AssignmentEndpoint {
     try (var connection = dataSource.getConnection()) {
       PreparedStatement statement =
           connection.prepareStatement(
-              "select password from challenge_users where userid = '"
-                  + username_login
-                  + "' and password = '"
-                  + password_login
-                  + "'");
+            "select password from challenge_users where 
+            userid = ? and password = ?"
+          );
+        statement.setString(1, username_login);
+        statement.setString(2, password_login);
       ResultSet resultSet = statement.executeQuery();
 
       if (resultSet.next()) {
         return success(this).feedback("challenge.solved").feedbackArgs(flags.getFlag(5)).build();
       } else {
-        return failed(this).feedback("challenge.close").build();
+        return failed(this).feedback("challenge.close").build();S
       }
     }
   }
